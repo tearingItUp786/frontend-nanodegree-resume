1d364f5ce79c3a938392f3ef22d1c07c1235b175
tearingItup786 <taranveer786@gmail.com>
2016-01-14 09:51:49 -0800

    Finished project, somewhat. Will be starting from scratch soon

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,8 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    var iName = inName() || function() {};
+    $('#name').html(iName);
   });
 });
 
@@ -74,18 +74,19 @@ The next few lines about clicks are for the Collecting Click Locations quiz in L
 */
 clickLocations = [];
 
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
+function logClicks(x, y) {
+  clickLocations.push({
+    x: x,
+    y: y
+  });
   console.log('x location: ' + x + '; y location: ' + y);
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x, y);
 });
 
 
@@ -95,7 +96,7 @@ This is the fun part. Here's where we generate the custom Google Map for the web
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
@@ -109,9 +110,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -121,7 +122,7 @@ function initializeMap() {
   written for bio, education, and work.
   */
   function locationFinder() {
-
+    console.log("Inside location finder");
     // initializes an empty array
     var locations = [];
 
@@ -130,17 +131,17 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    education.schools.forEach(function(school) {
       locations.push(school.location);
     });
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    work.jobs.forEach(function(job) {
       locations.push(job.location);
     });
 
@@ -153,12 +154,12 @@ function initializeMap() {
   about a single location.
   */
   function createMapMarker(placeData) {
-
+    console.log("Inside createMapMarker");
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat(); // latitude from the place service
+    var lon = placeData.geometry.location.lng(); // longitude from the place service
+    var name = placeData.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
@@ -176,7 +177,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -193,6 +194,7 @@ function initializeMap() {
   If so, it creates a new map marker for that location.
   */
   function callback(results, status) {
+    console.log("Inside callback");
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
@@ -203,13 +205,12 @@ function initializeMap() {
   and fires off Google place searches for each location
   */
   function pinPoster(locations) {
-
     // creates a Google place search service object. PlacesService does the work of
     // actually searching for location data.
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    locations.forEach(function(place) {
       // the search request object
       var request = {
         query: place
@@ -238,11 +239,13 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
+
+console.log("End of helper.js");
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,140 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+  "name": "Taran",
+  "age": 22,
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "555-555-5555",
+    "github": "tearingItUp786",
+    "email": "taranveer786@gmail.com",
+    "location":"Delta British Columbia"
+  },
+  "welcomeMessage": "Taran has been tearin' it up since 93 -- Mike Church",
+  "skills": [
+    "PHP", "HTML", "JavaScript", "CSS", "git"
+  ],
+  "bioPic": "images/fry.jpg"
+};
+
+var work = {
+  "jobs": [{
+    "title": "Business Analyst/Computer Technician",
+    "employer": "Beedie School of Business",
+    "location": "Burnaby",
+    "dates": "September 2014 - May 2015",
+    "description": "Aided in the development of a web platform to help manage internal processes and aided facutly/staff with computer related problems"
+  }, {
+    "title": "Web Developer",
+    "employer": "Kamal Masri",
+    "location": "Surrey",
+    "dates": "September 2015 - October 2015",
+    "description": "Acted as a front end web developer and created a website"
+  }]
+};
+
+var education = {
+  "schools": [{
+    "name": "SFU",
+    "": "Buranby",
+    "major": "Business",
+    "minor": "Computer Science",
+    "graduation": "2018"
+  }, {
+    "name": "UBC",
+    "city": "Vancouver",
+    "major": "Awesomeness",
+    "minor": "Computer Science",
+    "graduation": "2019"
+  }],
+  "onlineCourses": [{
+    "name": "Android Developer",
+    "major": "Android",
+    "graduation": "2014"
+  }, {
+    "name": "Body Language",
+    "major": "business",
+    "graduation": "2014"
+  }]
+};
+
+var projects = {
+  "projects": [{
+    "title": "Blog",
+    "dates": "September 2014 - Jan 2015",
+    "description": "Created a blog on my very own server",
+    "image": "images/Sephiroth-Cloud-final-fantasy-vii-29026371-1920-1200.png"
+  }, {
+    "title": "Photo Booth",
+    "dates": "January 2016 - February 2016",
+    "description": "Created a photo booth page",
+    "image": "images/Sephiroth-Cloud-final-fantasy-vii-29026371-1920-1200.png"
+  }]
+};
+
+$("#header").append(HTMLheaderName.replace("%data%", "Taranveer Bains"));
+$("#header").append(HTMLheaderRole.replace("%data%", "Web Developer"));
+
+if (bio.skills.length > 0) {
+
+  $("#header").append(HTMLskillsStart);
+  for (i = 0; i < bio.skills.length; i++) {
+    var skills = HTMLskills.replace("%data%", bio.skills[i]);
+    $("#skills").append(skills);
+  }
+}
+
+function displayWork() {
+  for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var formattedWorkDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+    $(".work-entry:last").append(formattedWorkEmployer + formattedWorkTitle + formattedWorkLocation + formattedWorkDate + formattedWorkDescription);
+  }
+}
+
+displayWork();
+
+function inName(name) {
+  name = name.trim().split(" ");
+  console.log(name);
+  name[0] = name[0].toLowerCase();
+  name[1] = name[1].toLowerCase();
+
+  name[0] = name[0][0].toUpperCase() + name[0].slice(1);
+  name[1] = name[1].toUpperCase();
+  name = name[0] + " " + name[1];
+  console.log(name);
+  return name;
+}
+
+inName("TARANVEER bains");
+$("#main").append(internationalizeButton);
+
+projects.display = function() {
+  for (project in projects.projects) {
+    $("#projects").append(HTMLprojectStart);
+
+    var title = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(title);
+
+    var date = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(date);
+
+    var descrip = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(descrip);
+
+    var pic = HTMLprojectImage.replace("%data%", projects.projects[project].image);
+    $(".project-entry:last").append(pic);
+  }
+};
+
+projects.display();
+
+$("#mapDiv").append(googleMap);
